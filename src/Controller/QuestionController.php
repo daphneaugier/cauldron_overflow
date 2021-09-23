a137132d817b2ad5c13f269f0f9a6868466781fe
Daphne Augier <35980232+daphneaugier@users.noreply.github.com>
2021-09-23 00:09:30 -0400

    feat: added simple css to site

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -13,7 +13,7 @@ class QuestionController extends AbstractController
      */
     public function homepage()
     {
-        return new Response('New controller created!');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
